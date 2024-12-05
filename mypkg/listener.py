d42d82b31b230da4932f776f0dbe30c974981961
Tanaka Kakeru <s23c1085lw@s.chibakoudai.jp>
2024-12-05 12:46:36 +0900

    lesson11 listenが途中

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -3,15 +3,16 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 
 
-rclpy.init()
-node = Node("listener")
+class Listener (Node): #listenerのクラス作る
+   def __init__(self): 
+      super().__init__("listener")
+      self.pub = self.create_subscription(Int16, "countup", self.cb, 10)
 
-
-def cb(msg):
-   global node
-   node.get_logger().info("Listen: %d" % msg.data)
+def cb(self, msg):
+   self.get_logger().info("Listen: %d" % msg.data)
 
 
 def main():
-    pub = node.create_subscription(Int16, "countup", cb, 10)
-    rclpy.spin(node)
+   rclpy.init()
+   node = Listener()
+   rclpy.spin(node)
--- mypkg/talker.py
+++ mypkg/talker.py
@@ -2,20 +2,21 @@ import rclpy        #ROS 2のクライアントのためのライブラリ
 from rclpy.node import Node      #ノードを実装するためのNodeクラス（クラスは第10回で）
 from std_msgs.msg import Int16   #通信の型（16ビットの符号付き整数）
 
-rclpy.init() 
-node = Node("talker")            #ノード作成（nodeという「オブジェクト」を作成）
-pub = node.create_publisher(Int16, "countup", 10)   #パブリッシャのオブジェクト作成
-n = 0 #カウント用変数
+### ヘッダの下にTalkerというクラスを作成 ###
+class Talker(Node):   #ヘッダの下にTalkerというクラスを作成
+    def __init__(self): #Nodeというクラスの機能を受け継いだクラスにな
+        super().__init__("takler") #Nodeのオブジェクトとしての初期化
+        self.pub = self.create_publisher(Int16, "countup", 10)  #selfはオブジェクトのこと
+        self.create_timer(0.5, self.cb)  #ここでタイマーをしかける。
+        self.n = 0   #オブジェクトにひとつパブリッシャと変数をもたせる。
 
-
-def cb():          #20行目で定期実行されるコールバック関数
-    global n       #関数を抜けてもnがリセットされないようにしている
-    msg = Int16()  #メッセージの「オブジェクト」
-    msg.data = n   #msgオブジェクトの持つdataにnを結び付け
-    pub.publish(msg)        #pubの持つpublishでメッセージ送信
-    n += 1
-
+    def cb(self):  #コールバックのメソッド
+        msg = Int16()
+        msg.data = self.n   #属性には必ずselfをつける
+        self.pub.publish(msg)
+        self.n += 1
 
 def main():
-    node.create_timer(0.5, cb)  #タイマー設定
-    rclpy.spin(node)         
+    rclpy.init()
+    node = Talker()
+    rclpy.spin(node)
